e4e98243540ba93143f4ed4b590fcb912eec48e8
Zoltan Kochan <z@kochan.io>
2022-01-23 17:23:58 +0200

    test: repeat install

--- e2e/harmony/root-components.e2e.ts
+++ e2e/harmony/root-components.e2e.ts
@@ -1,6 +1,7 @@
 import chai, { expect } from 'chai';
 import fs from 'fs-extra';
 import path from 'path';
+import { sync as resolveSync } from 'resolve';
 import Helper from '../../src/e2e-helper/e2e-helper';
 
 chai.use(require('chai-fs'));
@@ -8,15 +9,17 @@ chai.use(require('chai-fs'));
 describe('root components', function () {
   let helper: Helper;
   this.timeout(0);
+  let virtualStoreDir!: string;
+  let numberOfFilesInVirtualStore!: number;
   before(() => {
     helper = new Helper();
     helper.scopeHelper.reInitLocalScopeHarmony();
     helper.fixtures.populateComponents(4);
     helper.extensions.bitJsonc.addKeyValToDependencyResolver('rootComponents', ['@my-scope/comp3', '@my-scope/comp4']);
-    helper.fs.outputFile(`comp1/index.js`, ``);
-    helper.fs.outputFile(`comp2/index.js`, `const comp1 = require("@my-scope/comp1");`);
-    helper.fs.outputFile(`comp3/index.js`, `const comp2 = require("@my-scope/comp2");`);
-    helper.fs.outputFile(`comp4/index.js`, `const comp2 = require("@my-scope/comp2");`);
+    helper.fs.outputFile(`comp1/index.js`, `const React = require("react")`);
+    helper.fs.outputFile(`comp2/index.js`, `const React = require("react");const comp1 = require("@my-scope/comp1");`);
+    helper.fs.outputFile(`comp3/index.js`, `const React = require("react");const comp2 = require("@my-scope/comp2");`);
+    helper.fs.outputFile(`comp4/index.js`, `const React = require("react");const comp2 = require("@my-scope/comp2");`);
     helper.extensions.addExtensionToVariant('comp1', 'teambit.dependencies/dependency-resolver', {
       policy: {
         peerDependencies: {
@@ -46,6 +49,8 @@ describe('root components', function () {
       },
     });
     helper.command.install();
+    virtualStoreDir = path.join(helper.fixtures.scopes.localPath, 'node_modules/.pnpm');
+    numberOfFilesInVirtualStore = fs.readdirSync(virtualStoreDir).length;
   });
   after(() => {
     helper.scopeHelper.destroy();
@@ -75,7 +80,7 @@ describe('root components', function () {
       ).version
     ).to.match(/^17\./);
   });
-  it('should install the dependencies of the root component that has react 16 in the dependencies with react 16', function () {
+  it('should install the dependencies of the root component that has react 16 in the dependencies with react 16', () => {
     expect(
       fs.readJsonSync(
         resolveFrom(helper.fixtures.scopes.localPath, [
@@ -96,28 +101,168 @@ describe('root components', function () {
       ).version
     ).to.match(/^16\./);
   });
-  it('should install the non-root components with their default React versions', function () {
+  it('should install the non-root components with their default React versions', () => {
     expect(
-      fs.readJsonSync(resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp1/index.js', 'react/package.json']))
-        .version
+      fs.readJsonSync(
+        resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp1/index.js', 'react/package.json'])
+      ).version
     ).to.match(/^17\./);
     expect(
-      fs.readJsonSync(resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp2/index.js', 'react/package.json']))
-        .version
+      fs.readJsonSync(
+        resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp2/index.js', 'react/package.json'])
+      ).version
     ).to.match(/^17\./);
     expect(
-      fs.readJsonSync(resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp3/index.js', 'react/package.json']))
-        .version
+      fs.readJsonSync(
+        resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp3/index.js', 'react/package.json'])
+      ).version
     ).to.match(/^16\./);
     expect(
-      fs.readJsonSync(resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp4/index.js', 'react/package.json']))
-        .version
+      fs.readJsonSync(
+        resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp4/index.js', 'react/package.json'])
+      ).version
     ).to.match(/^17\./);
+  });
+  it('should create package.json file in every variation of the component', () => {
+    let pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+      '@my-scope/comp3__root',
+      '@my-scope/comp2/package.json',
+    ])
+    expect(pkgJsonLoc).to.contain('.pnpm');
+    expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp2');
+
+    pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+      '@my-scope/comp4__root',
+      '@my-scope/comp2/package.json',
+    ])
+    expect(pkgJsonLoc).to.contain('.pnpm');
+    expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp2');
+
+    pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+      '@my-scope/comp3__root',
+      '@my-scope/comp2',
+      '@my-scope/comp1/package.json',
+    ])
+    expect(pkgJsonLoc).to.contain('.pnpm');
+    expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp1');
+
+    pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+      '@my-scope/comp4__root',
+      '@my-scope/comp2',
+      '@my-scope/comp1/package.json',
+    ])
+    expect(pkgJsonLoc).to.contain('.pnpm');
+    expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp1');
+  });
+  describe('repeat install', () => {
+    before(() => {
+      helper.command.install();
+    });
+    it('should not add new or remove old deps', () => {
+      expect(fs.readdirSync(virtualStoreDir).length).to.eq(numberOfFilesInVirtualStore);
+    });
+    it('should install the dependencies of the root component that has react 17 in the dependencies with react 17', function () {
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, [
+            '@my-scope/comp4__root',
+            '@my-scope/comp2',
+            'react/package.json',
+          ])
+        ).version
+      ).to.match(/^17\./);
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, [
+            '@my-scope/comp4__root',
+            '@my-scope/comp2',
+            '@my-scope/comp1',
+            'react/package.json',
+          ])
+        ).version
+      ).to.match(/^17\./);
+    });
+    it('should install the dependencies of the root component that has react 16 in the dependencies with react 16', () => {
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, [
+            '@my-scope/comp3__root',
+            '@my-scope/comp2',
+            'react/package.json',
+          ])
+        ).version
+      ).to.match(/^16\./);
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, [
+            '@my-scope/comp3__root',
+            '@my-scope/comp2',
+            '@my-scope/comp1',
+            'react/package.json',
+          ])
+        ).version
+      ).to.match(/^16\./);
+    });
+    it('should install the non-root components with their default React versions', () => {
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp1/index.js', 'react/package.json'])
+        ).version
+      ).to.match(/^17\./);
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp2/index.js', 'react/package.json'])
+        ).version
+      ).to.match(/^17\./);
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp3/index.js', 'react/package.json'])
+        ).version
+      ).to.match(/^16\./);
+      expect(
+        fs.readJsonSync(
+          resolveFrom(helper.fixtures.scopes.localPath, ['@my-scope/comp4/index.js', 'react/package.json'])
+        ).version
+      ).to.match(/^17\./);
+    });
+    it('should create package.json file in every variation of the component', () => {
+      let pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+        '@my-scope/comp3__root',
+        '@my-scope/comp2/package.json',
+      ])
+      expect(pkgJsonLoc).to.contain('.pnpm');
+      expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp2');
+
+      pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+        '@my-scope/comp4__root',
+        '@my-scope/comp2/package.json',
+      ])
+      expect(pkgJsonLoc).to.contain('.pnpm');
+      expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp2');
+
+      pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+        '@my-scope/comp3__root',
+        '@my-scope/comp2',
+        '@my-scope/comp1/package.json',
+      ])
+      expect(pkgJsonLoc).to.contain('.pnpm');
+      expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp1');
+
+      pkgJsonLoc = resolveFrom(helper.fixtures.scopes.localPath, [
+        '@my-scope/comp4__root',
+        '@my-scope/comp2',
+        '@my-scope/comp1/package.json',
+      ])
+      expect(pkgJsonLoc).to.contain('.pnpm');
+      expect(fs.readJsonSync(pkgJsonLoc).name).to.eq('@my-scope/comp1');
+    });
   });
 });
 
 function resolveFrom(fromDir: string, moduleIds: string[]) {
   if (moduleIds.length === 0) return fromDir;
   const [moduleId, ...rest] = moduleIds;
-  return resolveFrom(require.resolve(moduleId, { paths: [fromDir] }), rest);
+  // We use the "resolve" library because the native "require.resolve" method uses a cache.
+  // So with the native resolve method we cannot check the same path twice.
+  return resolveFrom(resolveSync(moduleId, { basedir: fromDir, preserveSymlinks: false }), rest);
 }
